72a34b10f97f4577b5236f77513a9360d6885f02
bbwwpark <bbwwpark@naver.com>
2022-02-11 17:14:40 +0900

    refactor: 할 일 완료 기능 리팩터링

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -21,11 +21,7 @@ export default {
       this.$emit('removeItem', todoItem, index);
     } ,
     toggleComplete: function(todoItem, index) {
-      console.log(todoItem, index);
-      todoItem.completed = !todoItem.completed;
-
-      localStorage.removeItem(todoItem.item)
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+      this.$emit('toggleItem', todoItem, index);
     }
   }
 }
